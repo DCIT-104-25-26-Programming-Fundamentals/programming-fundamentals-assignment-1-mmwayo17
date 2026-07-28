d160ba4bf8512630e4b7045a58d10950e7a05b1b
mmwayo17 <mmwayo_seini001@st.ug.edu.gh>
2026-07-28 14:35:55 +0000

    finished assignment 9

--- assignment_09_simple_calculator.py
+++ assignment_09_simple_calculator.py
@@ -67,4 +67,74 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
+from time import sleep #I'm importing it just to make sure you see the goodbye message.
 
+def addition(number1, number2):
+    return number1 + number2
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if num2 == 0:
+        return "Second Number cannot be zero"
+    return round(num1/num2, 2)
+
+def modulus(num1, num2):
+    if num2 == 0: return "Second Number cannot be zero"
+    return num1 % num2
+
+def expo(num1, num2):
+    return num1 ** num2
+
+def main():
+    while True:
+        print(
+            """
+    ============================
+         SIMPLE CALCULATOR
+    ============================
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division
+    5. Modulus
+    6. Exponentiation
+    7. Quit
+    """
+        )
+        try:
+            choice = int(input("Select an operation (1-7):"))
+            if choice == 7:
+                print("Goodbye!✌️")
+                sleep(2)
+                return False
+
+            if choice not in range(8):
+                print("Out of range. Must be between 1 - 7")
+                continue
+
+            number1 = int(input("Enter first number: "))
+            number2 = int(input("Enter second number: "))
+
+            if choice == 1:
+                print(addition(number1, number2))
+            elif choice == 2:
+                print(subtraction(number1, number2))
+            elif choice == 3:
+                print(multiplication(number1, number2))
+            elif choice == 4:
+                print(division(number1, number2))
+            elif choice == 5:
+                print(modulus(number1, number2))
+            elif choice == 6:
+                print(expo(number1, number2))
+        except ValueError:
+            print("Invalid Input must be an integer.")
+        
+
+if __name__ == "__main__":
+    main()
